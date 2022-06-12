2ffcffa27f13a98c4af7615f7e8796cdf73d5069
sena <hargiantofebrisena@gmail.com>
2022-06-12 22:04:46 +0700

    initial

--- main.go
+++ main.go
@@ -16,9 +16,9 @@ func main() {
 
 	app.Get("/consume/wallet", controller.ConsumeWallet)
 	app.Post("/create/wallet", controller.PublishWallet)
+	app.Get("/wallet", middlewares.AuthRequired(), controller.Wallet)
 
 	app.Post("/login", controller.Login)
-	app.Get("/wallet", middlewares.AuthRequired(), controller.Wallet)
 
 	err := app.Listen(3000)
 	if err != nil {
